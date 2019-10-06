09592b2e768259da9e9ccddb1e1efe2f388116e0
Rilwan Adams <adamzrealone@gmail.com>
2019-10-06 19:48:16 +0100

    Gulp: tailwind recompile on change

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ const browserSync = require('browser-sync').create();
 const gulp = require('gulp');
 
 gulp.task('watch', function() {
-  gulp.watch('src/tailwind.css', ['css'], browserSync.reload);
+  gulp.watch('src/*.css', gulp.series('css'));
+  gulp.watch('tailwind.config.js', gulp.series('css'));
+  gulp.watch('postcss.config.js', gulp.series('css'));
   gulp.watch('src/*.html', browserSync.reload);
 });
 
@@ -13,6 +15,9 @@ gulp.task('browser-sync', function() {
     },
   });
 
+  gulp.watch('tailwind.config.js', gulp.series('css'));
+  gulp.watch('src/tailwind.css', gulp.series('css'));
+  gulp.watch('postcss.config.js', gulp.series('css'));
   gulp.watch('src/css/*.css').on('change', browserSync.reload);
   gulp.watch('src/*.html').on('change', browserSync.reload);
 });
@@ -23,7 +28,8 @@ gulp.task('css', function() {
   return gulp
     .src('src/tailwind.css')
     .pipe(postcss([require('tailwindcss'), require('autoprefixer')]))
-    .pipe(gulp.dest('src/css'));
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('default', gulp.series('css', 'browser-sync', 'watch'));
