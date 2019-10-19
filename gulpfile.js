3331aea895a9b871c08b47510c8da27d546b2503
Rilwan Adams <adamzrealone@gmail.com>
2019-10-19 10:29:56 +0100

    Feature: brotli, minify and purge added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
 const browserSync = require('browser-sync').create();
 const gulp = require('gulp');
 
+const env = 'dev';
+const dest = 'src/css';
+const src = 'src/css/tailwind.css';
+
+const plugins = [
+  require('tailwindcss'),
+  require('autoprefixer'),
+  ...(env === 'production'
+    ? [
+        require('@fullhuman/postcss-purgecss')({
+          content: ['./src/**/*.html', './src/**/*.tsx'],
+          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        }),
+      ]
+    : []),
+];
+
 gulp.task('watch', function() {
   gulp.watch('src/*.css', gulp.series('css'));
   gulp.watch('tailwind.config.js', gulp.series('css'));
@@ -27,9 +44,32 @@ gulp.task('css', function() {
 
   return gulp
     .src('src/tailwind.css')
-    .pipe(postcss([require('tailwindcss'), require('autoprefixer')]))
-    .pipe(gulp.dest('src/css'))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest(dest))
     .pipe(browserSync.stream());
 });
+
+gulp.task('brotli', function() {
+  const brotli = require('gulp-brotli');
+
+  return gulp
+    .src(src)
+    .pipe(brotli.compress())
+    .pipe(gulp.dest(dest));
+});
+
+gulp.task('minify', function() {
+  const cleanCss = require('gulp-clean-css');
+  const sourcemaps = require('gulp-sourcemaps');
+
+  return gulp
+    .src(src)
+    .pipe(sourcemaps.init())
+    .pipe(cleanCss())
+    .pipe(sourcemaps.write(''))
+    .pipe(gulp.dest(dest));
+});
+
+gulp.task('prod', gulp.series('css', 'minify', 'brotli'));
 
 gulp.task('default', gulp.series('css', 'browser-sync', 'watch'));
